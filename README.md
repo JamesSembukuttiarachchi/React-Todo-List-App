8bcfdc01c76b1cee70e7b63ee6635b7392f525a1
James Sembukuttiarachchi <jamessanjayasem@gmail.com>
2024-01-10 12:03:47 +0530

    commit all

---
+++ README.md
@@ -0,0 +1,2 @@
+# React-Todo-List-App
+A React-based Todo-App designed for efficient task management. This repository contains a user-friendly and intuitive Todo-App built using React, enabling users to create, track, and organize their tasks seamlessly. The application offers features for adding, editing, and deleting tasks.
---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+
+  const [value, setValue] = useState(task.task)
+  const handlesubmit = e => {
+    e.preventDefault();
+    editTodo(value, task.id);
+    setValue("");
+  }
+  return (
+    <form className='TodoForm' onSubmit={handlesubmit}>
+      <input type='text' className='todo-input' value={value} placeholder='update task' 
+      onChange={(e) => setValue(e.target.value)}/>     
+      <button type='submit' className='todo-btn'>Update Task</button>
+    </form>
+  )
+}
+
+export default EditTodoForm
